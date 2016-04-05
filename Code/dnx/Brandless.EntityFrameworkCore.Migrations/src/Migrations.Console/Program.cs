27a7d91ed78bdbb673de80716d56150c62115774
Josh Comley <joshcomley@googlemail.com>
2016-04-06 00:24:04 +0100

    Removed reference to now missing System.AppContext

--- Code/dnx/Brandless.EntityFrameworkCore.Migrations/src/Migrations.Console/Program.cs
+++ Code/dnx/Brandless.EntityFrameworkCore.Migrations/src/Migrations.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Brandless.EntityFrameworkCore.Migrations;
+using Microsoft.DotNet.Cli.Utils;
 using Migrations.DataContext;
 
 namespace Migrations
@@ -9,7 +10,10 @@ namespace Migrations
 	{
 		public static void Main(string[] args)
 		{
-			var projectPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\Migrations.DataContext"));
+			//var projectPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\Migrations.DataContext"));
+			var projectPath =
+				@"D:\B\BrandlessOpen\Code\dnx\Brandless.EntityFrameworkCore.Migrations\src\Migrations.DataContext";
+			Console.WriteLine("Running in " + projectPath.Yellow());
 			var codeFirstMigrations = new CodeFirstMigrations<AppDbContext>(projectPath);
 			codeFirstMigrations.Add("Init");
 		}
